e13c867fe03be906308d7fbf3669e0eae2ba9be9
lgancarczyk <lukgan2000@gmail.com>
2021-05-26 14:54:35 +0200

    zajj

--- projekty/YT_downloader/flask_downloader/app.py
+++ projekty/YT_downloader/flask_downloader/app.py
@@ -14,18 +14,26 @@ def home():
     form = LinkForm()
     if form.validate_on_submit():
         
-        # flash(f"fuck you")
         
-        link = form.link.data
-        thumbnail_url = thumbnailUrl(link)
-        session['thumbnail_url'] = thumbnail_url
-        print(thumbnail_url)
-        return redirect(url_for('about'))
+        try:
+            link = form.link.data
+            session['thumbnail_url'] = thumbnailUrl(link)
+            session['video_name'] = videoName(link)
+            session['video_views'] = videoViews(link)
+            return redirect(url_for('about'))
+        except:
+            flash(f"Enter valid link!")
+            return redirect(url_for('home'))
+        
     return render_template('home.html', form=form)
 
 @app.route("/about", methods=['GET', 'POST'])
 def about(thumbnail_url=None):
-    return render_template('about.html', thumbnail_url=session['thumbnail_url'])
+    return render_template('about.html',
+                                        thumbnail_url=session['thumbnail_url'], 
+                                        video_views = session['video_views'],
+                                        video_name = session['video_name']
+                                        )
 
 
 if __name__ == '__main__':
--- projekty/YT_downloader/flask_downloader/downloadscript.py
+++ projekty/YT_downloader/flask_downloader/downloadscript.py
@@ -5,3 +5,10 @@ def thumbnailUrl(link):
     yt = YouTube(link)
     return yt.thumbnail_url
 
+def videoName(link):
+    yt = YouTube(link)
+    return yt.title
+
+def videoViews(link):
+    yt = YouTube(link)
+    return yt.views
