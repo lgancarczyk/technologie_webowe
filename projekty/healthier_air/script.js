6ad63a03600a95f2d79c862bd48201b6fa988539
lgancarczyk <lukgan2000@gmail.com>
2021-06-10 00:18:16 +0200

    inny slideshow przed

--- projekty/healthier_air/script.js
+++ projekty/healthier_air/script.js
@@ -1,4 +1,3 @@
-
 const hamburger_btn = document.querySelector('.hamburger_btn');
 const header_mobile_links = document.querySelector('#header_mobile_links');
 
@@ -7,5 +6,17 @@ hamburger_btn.addEventListener('click',()=>{
     header_mobile_links.classList.toggle('header_mobile_links_open');
     header_mobile_links.classList.toggle('header_mobile_links_closed')
 })
+
+
+// var counter = 1;
+//     setInterval(function(){
+//       document.getElementById('radio' + counter).checked = true;
+//       counter++;
+//       if(counter > 4){
+//         counter = 1;
+//       }
+//     }, 5000);
+
+
 
 
