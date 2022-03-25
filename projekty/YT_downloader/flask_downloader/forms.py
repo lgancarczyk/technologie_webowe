ca330f13d0fe5e5e790523b653db08edf5eb1ef1
lgancarczyk <79982113+lgancarczyk@users.noreply.github.com>
2022-03-25 23:41:58 +0100

    Delete projekty/YT_downloader directory

--- projekty/YT_downloader/flask_downloader/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-
-class LinkForm(FlaskForm):
-    link = StringField('Link', validators=[DataRequired(), Length(min=10)])
-    submit = SubmitField('Find')
